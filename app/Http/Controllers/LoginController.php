0df4181e06090c6db343536f4dd7887e052a9c56
kierth <villegaskierthryan@gmail.com>
2023-11-14 13:59:18 +0800

    I created login backend

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -2,6 +2,9 @@
 
 namespace App\Http\Controllers;
 
+use Illuminate\Http\Response;
+use App\Http\Requests\LoginRequest;
+use App\Http\Resources\LoginResource;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 
@@ -10,9 +13,12 @@ class LoginController extends Controller
     public function __invoke(Request $request)
     {
         $credentials = $request->only("email", "password");
-
         if (Auth::attempt($credentials)) {
             $user = Auth::user();
+            $token = $user->createToken("token")->plainTextToken;
+            return (new LoginResource($user))->additional(["token" => $token]);
         }
+
+        return response()->json(array("message" => "email or password is incorrect"), Response::HTTP_UNAUTHORIZED);
     }
 }
